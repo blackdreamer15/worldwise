d4593be66e34d0bcdef3ac0a66c8bc75d06fbb7c
Jessy Baki <bakijessy@gmail.com>
2023-09-19 16:52:00 +0000

    Add content to Pricing.jsx

--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -1,5 +1,21 @@
-function Pricing() {
-  return <div></div>;
+export default function Pricing() {
+  return (
+    <main>
+      <section>
+        <div>
+          <h2>
+            Simple pricing.
+            <br />
+            Just $9/month.
+          </h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae vel
+            labore mollitia iusto. Recusandae quos provident, laboriosam fugit
+            voluptatem iste.
+          </p>
+        </div>
+        <img src="img-2.jpg" alt="overview of a large city with skyscrapers" />
+      </section>
+    </main>
+  );
 }
-
-export default Pricing;
