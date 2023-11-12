9e433f559b5cfbb478507538eca2ae783d42959c
blackdreamer15 <bakijessy@gmail.com>
2023-11-12 20:49:44 +0000

    Chore: Comment unused code

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,29 +1,29 @@
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { useNavigate /*useSearchParams*/ } from "react-router-dom";
 import styles from "./Map.module.css";
 
 function Map() {
-  const navigate = useNavigate();
-  const [searchParams, setSearchParams] = useSearchParams();
+    const navigate = useNavigate();
+    // const [searchParams, setSearchParams] = useSearchParams();
 
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
+    // const lat = searchParams.get("lat");
+    // const lng = searchParams.get("lng");
 
-  return (
-    <div
-      className={styles.mapContainer}
-      onClick={() => {
-        navigate("form");
-      }}
-    >
-      <h2>
+    return (
+        <div
+            className={styles.mapContainer}
+            onClick={() => {
+                navigate("form");
+            }}
+        >
+            {/* <h2>
         lat:{lat} and lng: {lng}
       </h2>
 
       <button onClick={() => setSearchParams({ lat: 23, lng: 25 })}>
         Change PARMS
-      </button>
-    </div>
-  );
+      </button> */}
+        </div>
+    );
 }
 
 export default Map;
