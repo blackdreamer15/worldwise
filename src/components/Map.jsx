c9731c92d1b5af23513fbbab40de76716184c461
blackdreamer15 <bakijessy@gmail.com>
2023-11-12 21:46:06 +0000

    Add some styles to the map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -22,6 +22,7 @@ function Map() {
                 center={mapPosition}
                 zoom={13}
                 scrollWheelZoom={false}
+                className={styles.map}
             >
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
