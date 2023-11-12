60b18f4d3f6b921c6d94e0a5a9ac3cf8a0249a68
blackdreamer15 <bakijessy@gmail.com>
2023-11-12 21:38:03 +0000

    Fix: Pass the mapPosition state to the map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -18,12 +18,16 @@ function Map() {
                 navigate("form");
             }}
         >
-            <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+            <MapContainer
+                center={mapPosition}
+                zoom={13}
+                scrollWheelZoom={false}
+            >
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={position}>
+                <Marker position={mapPosition}>
                     <Popup>
                         A pretty CSS3 popup. <br /> Easily customizable.
                     </Popup>
