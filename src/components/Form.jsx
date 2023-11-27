d6dbd3843ea5cf7f756986e4fd8eba4b31004522
blackdreamer15 <bakijessy@gmail.com>
2023-11-27 17:04:02 +0000

    Ficx: Import useEffect

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,6 @@
 // "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=0&longitude=0"
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import styles from "./Form.module.css";
 import Button from "./Button";
