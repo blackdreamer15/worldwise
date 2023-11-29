cba14d58eecc99889aaca770f73c91c111ba4deb
blackdreamer15 <bakijessy@gmail.com>
2023-11-29 22:31:36 +0000

    Chore: Create FakeAuthContext

---
+++ src/contexts/FakeAuthContext.jsx
@@ -0,0 +1,18 @@
+import { createContext, useContext } from "react";
+
+const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+    return <AuthContext.Provider>{children}</AuthContext.Provider>;
+};
+
+function useAuth() {
+    const context = useContext(AuthContext);
+
+    if (context === undefined)
+        throw new Error("AuthContext was used outside the AuhProvider");
+
+    return context;
+}
+
+export { AuthProvider, useAuth };
