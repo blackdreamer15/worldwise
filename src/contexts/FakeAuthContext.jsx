11dae11440a347c0bc37770f5f73e35f647bcc30
blackdreamer15 <bakijessy@gmail.com>
2023-11-29 22:41:14 +0000

    Chore: Add fake user data

--- src/contexts/FakeAuthContext.jsx
+++ src/contexts/FakeAuthContext.jsx
@@ -2,6 +2,8 @@ import { createContext, useContext, useReducer } from "react";
 
 const AuthContext = createContext();
 
+const initialState = { user: null, isAuthenticated: false };
+
 function reducer(state, action) {
     switch (action.type) {
         case "login":
@@ -13,7 +15,12 @@ function reducer(state, action) {
     }
 }
 
-const initialState = { user: null, isAuthenticated: false };
+const FAKE_USER = {
+    name: "Jack",
+    email: "jack@example.com",
+    password: "qwerty",
+    avatar: "https://i.pravatar.cc/100?u=zz",
+};
 
 const AuthProvider = ({ children }) => {
     const [{ user, isAuthenticated }, dispatch] = useReducer(initialState);
