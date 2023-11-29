2f4e9ab0bd22fc3ddfaef48063ee8ed6c04fc8bb
blackdreamer15 <bakijessy@gmail.com>
2023-11-29 18:11:49 +0000

    Chore: Consume deleteCity from useCities hook

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -4,39 +4,46 @@ import styles from "./CityItem.module.css";
 import { useCities } from "../contexts/CitiesContext";
 
 const formatDate = (date) =>
-  new Intl.DateTimeFormat("en-US", {
-    day: "numeric",
-    month: "long",
-    year: "numeric",
-  }).format(new Date(date));
+    new Intl.DateTimeFormat("en-US", {
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+    }).format(new Date(date));
 
 /*eslint-disable react/prop-types*/
 function CityItem({ city }) {
-  const {
-    emoji,
-    cityName,
-    date,
-    id,
-    position: { lat, lng },
-  } = city;
+    const {
+        emoji,
+        cityName,
+        date,
+        id,
+        position: { lat, lng },
+    } = city;
 
-  const { currentCity } = useCities();
+    const { currentCity, deleteCity } = useCities();
 
-  return (
-    <li>
-      <Link
-        className={`${styles.cityItem} ${
-          id === currentCity.id && styles["cityItem--active"]
-        }`}
-        to={`${id}?lat=${lat}&lng=${lng}`}
-      >
-        <span className={styles.emoji}>{emoji}</span>
-        <span className={styles.name}>{cityName}</span>
-        <time className={styles.date}>{formatDate(date || null)}</time>
-        <button className={styles.deleteBtn}>&times;</button>
-      </Link>
-    </li>
-  );
+    function handleClick(e) {
+        e.preventDefault();
+        deleteCity(id);
+    }
+
+    return (
+        <li>
+            <Link
+                className={`${styles.cityItem} ${
+                    id === currentCity.id && styles["cityItem--active"]
+                }`}
+                to={`${id}?lat=${lat}&lng=${lng}`}
+            >
+                <span className={styles.emoji}>{emoji}</span>
+                <span className={styles.name}>{cityName}</span>
+                <time className={styles.date}>{formatDate(date || null)}</time>
+                <button className={styles.deleteBtn} onClick={handleClick}>
+                    &times;
+                </button>
+            </Link>
+        </li>
+    );
 }
 
 export default CityItem;
