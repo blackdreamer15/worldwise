83dd8bd6698dbe25d9ae63b8371552fee09828e1
blackdreamer15 <bakijessy@gmail.com>
2023-12-01 22:54:43 +0000

    Chore: Consume the context un Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,41 +1,44 @@
 import { useState } from "react";
 import styles from "./Login.module.css";
 import PageNav from "../components/PageNav";
+import { useAuth } from "../contexts/FakeAuthContext";
 
 export default function Login() {
-  // PRE-FILL FOR DEV PURPOSES
-  const [email, setEmail] = useState("jack@example.com");
-  const [password, setPassword] = useState("qwerty");
+    const { login } = useAuth();
 
-  return (
-    <main className={styles.login}>
-      <PageNav />
+    // PRE-FILL FOR DEV PURPOSES
+    const [email, setEmail] = useState("jack@example.com");
+    const [password, setPassword] = useState("qwerty");
 
-      <form className={styles.form}>
-        <div className={styles.row}>
-          <label htmlFor="email">Email address</label>
-          <input
-            type="email"
-            id="email"
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-          />
-        </div>
+    return (
+        <main className={styles.login}>
+            <PageNav />
 
-        <div className={styles.row}>
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
-          />
-        </div>
+            <form className={styles.form} onSubmit={login}>
+                <div className={styles.row}>
+                    <label htmlFor="email">Email address</label>
+                    <input
+                        type="email"
+                        id="email"
+                        onChange={(e) => setEmail(e.target.value)}
+                        value={email}
+                    />
+                </div>
 
-        <div>
-          <button>Login</button>
-        </div>
-      </form>
-    </main>
-  );
+                <div className={styles.row}>
+                    <label htmlFor="password">Password</label>
+                    <input
+                        type="password"
+                        id="password"
+                        onChange={(e) => setPassword(e.target.value)}
+                        value={password}
+                    />
+                </div>
+
+                <div>
+                    <button>Login</button>
+                </div>
+            </form>
+        </main>
+    );
 }
