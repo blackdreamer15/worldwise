8a0dca9b55698a06c0a9a75049b2cf89e29e6db8
blackdreamer15 <bakijessy@gmail.com>
2023-12-01 23:33:33 +0000

    Fix: import from react-router-dom instead of react

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,5 @@
-import { useState, useNavigate, useEffect } from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import styles from "./Login.module.css";
 import PageNav from "../components/PageNav";
 import { useAuth } from "../contexts/FakeAuthContext";
