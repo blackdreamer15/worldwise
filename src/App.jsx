c31abb87a0d293f88c7d0562012ee3ae98297976
blackdreamer15 <bakijessy@gmail.com>
2023-12-02 22:31:24 +0000

    chore: Protect the AppLayout page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
+import { CitiesProvider } from "./contexts/CitiesContext";
+import { AuthProvider } from "./contexts/FakeAuthContext";
+import ProtectedRoute from "./pages/ProtectedRoute";
+
 import Homepage from "./pages/Homepage";
 import Pricing from "./pages/Pricing";
 import Product from "./pages/Product";
@@ -10,35 +14,37 @@ import Form from "./components/Form";
 import City from "./components/City";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
-import { CitiesProvider } from "./contexts/CitiesContext";
-import { AuthProvider } from "./contexts/FakeAuthContext";
 
 function App() {
-    return (
-        <AuthProvider>
-            <CitiesProvider>
-                <BrowserRouter>
-                    <Routes>
-                        <Route path="/" element={<Homepage />} />
-                        <Route path="pricing" element={<Pricing />} />
-                        <Route path="product" element={<Product />} />
-                        <Route path="login" element={<Login />} />
-                        <Route path="app" element={<AppLayout />}>
-                            <Route
-                                index
-                                element={<Navigate replace to="cities" />}
-                            />
-                            <Route path="cities" element={<CityList />} />
-                            <Route path="cities/:id" element={<City />} />
-                            <Route path="countries" element={<CountryList />} />
-                            <Route path="form" element={<Form />} />
-                        </Route>
-                        <Route path="*" element={<PageNotFound />} />
-                    </Routes>
-                </BrowserRouter>
-            </CitiesProvider>
-        </AuthProvider>
-    );
+	return (
+		<AuthProvider>
+			<CitiesProvider>
+				<BrowserRouter>
+					<Routes>
+						<Route path="/" element={<Homepage />} />
+						<Route path="pricing" element={<Pricing />} />
+						<Route path="product" element={<Product />} />
+						<Route path="login" element={<Login />} />
+						<Route
+							path="app"
+							element={
+								<ProtectedRoute>
+									<AppLayout />
+								</ProtectedRoute>
+							}
+						>
+							<Route index element={<Navigate replace to="cities" />} />
+							<Route path="cities" element={<CityList />} />
+							<Route path="cities/:id" element={<City />} />
+							<Route path="countries" element={<CountryList />} />
+							<Route path="form" element={<Form />} />
+						</Route>
+						<Route path="*" element={<PageNotFound />} />
+					</Routes>
+				</BrowserRouter>
+			</CitiesProvider>
+		</AuthProvider>
+	);
 }
 
 export default App;
