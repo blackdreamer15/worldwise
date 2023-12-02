3ca35644d4e3f1c1615bc6e0578ab6d9701eea84
blackdreamer15 <bakijessy@gmail.com>
2023-12-02 21:29:35 +0000

    chore: Add getCity to dependency array

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -7,88 +7,88 @@ import { useEffect } from "react";
 import BackButton from "./BackButton";
 
 const formatDate = (date) =>
-  new Intl.DateTimeFormat("en", {
-    day: "numeric",
-    month: "long",
-    year: "numeric",
-    weekday: "long",
-  }).format(new Date(date));
+	new Intl.DateTimeFormat("en", {
+		day: "numeric",
+		month: "long",
+		year: "numeric",
+		weekday: "long",
+	}).format(new Date(date));
 
 function City() {
-  // TEMP DATA
-  // const currentCity = {
-  //   cityName: "Lisbon",
-  //   emoji: "🇵🇹",
-  //   date: "2027-10-31T15:59:59.138Z",
-  //   notes: "My favorite city so far!",
-  // };
+	// TEMP DATA
+	// const currentCity = {
+	//   cityName: "Lisbon",
+	//   emoji: "🇵🇹",
+	//   date: "2027-10-31T15:59:59.138Z",
+	//   notes: "My favorite city so far!",
+	// };
 
-  // const [searchParams] = useSearchParams();
+	// const [searchParams] = useSearchParams();
 
-  // const lat = searchParams.get("lat");
-  // const lng = searchParams.get("lng");
+	// const lat = searchParams.get("lat");
+	// const lng = searchParams.get("lng");
 
-  const { id } = useParams();
+	const { id } = useParams();
 
-  const { getCity, currentCity, isLoading } = useCities();
+	const { getCity, currentCity, isLoading } = useCities();
 
-  const { cityName, emoji, date, notes } = currentCity;
+	const { cityName, emoji, date, notes } = currentCity;
 
-  useEffect(
-    function () {
-      getCity(id);
-    },
-    [id]
-  );
+	useEffect(
+		function () {
+			getCity(id);
+		},
+		[id, getCity]
+	);
 
-  if (isLoading) return <Spinner />;
-  // return (
-  //   <>
-  //     <h1>CITY</h1>
-  //     <h2>id: {id}</h2>
-  //     <h2>
-  //       lat: {lat} and lng: {lng}
-  //     </h2>
-  //   </>
-  // );
+	if (isLoading) return <Spinner />;
+	// return (
+	//   <>
+	//     <h1>CITY</h1>
+	//     <h2>id: {id}</h2>
+	//     <h2>
+	//       lat: {lat} and lng: {lng}
+	//     </h2>
+	//   </>
+	// );
 
-  return (
-    <div className={styles.city}>
-      <div className={styles.row}>
-        <h6>City name</h6>
-        <h3>
-          <span>{emoji}</span> {cityName}
-        </h3>
-      </div>
+	return (
+		<div className={styles.city}>
+			<div className={styles.row}>
+				<h6>City name</h6>
+				<h3>
+					<span>{emoji}</span> {cityName}
+				</h3>
+			</div>
 
-      <div className={styles.row}>
-        <h6>You went to {cityName} on</h6>
-        <p>{formatDate(date || null)}</p>
-      </div>
+			<div className={styles.row}>
+				<h6>You went to {cityName} on</h6>
+				<p>{formatDate(date || null)}</p>
+			</div>
 
-      {notes && (
-        <div className={styles.row}>
-          <h6>Your notes</h6>
-          <p>{notes}</p>
-        </div>
-      )}
+			{notes && (
+				<div className={styles.row}>
+					<h6>Your notes</h6>
+					<p>{notes}</p>
+				</div>
+			)}
 
-      <div className={styles.row}>
-        <h6>Learn more</h6>
-        <a
-          href={`https://en.wikipedia.org/wiki/${cityName}`}
-          target="_blank"
-          rel="noreferrer"
-        >
-          Check out {cityName} on Wikipedia &rarr;
-        </a>
-      </div>
+			<div className={styles.row}>
+				<h6>Learn more</h6>
+				<a
+					href={`https://en.wikipedia.org/wiki/${cityName}`}
+					target="_blank"
+					rel="noreferrer"
+				>
+					Check out {cityName} on Wikipedia &rarr;
+				</a>
+			</div>
 
-      <div>
-        <BackButton />
-      </div>
-    </div>
-  );
+			<div>
+				<BackButton />
+			</div>
+		</div>
+	);
 }
 
 export default City;
